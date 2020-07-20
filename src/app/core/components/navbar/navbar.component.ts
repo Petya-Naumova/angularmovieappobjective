585f6e9dfd0166fdc5a743fbee69076a2e097949
Petya Naumova <petianaumova@gmail.com>
2020-07-20 04:47:20 +0300

    Add login page and hide the navbar when user is on the login page

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationStart, Router} from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -6,8 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+  navbarVisible = false;
+  constructor(private router: Router) {
+    router.events.forEach((event) => {
+      if (event instanceof NavigationStart) {
+        if (event.url === '/login') {
+          this.navbarVisible = false;
+        } else {
+          this.navbarVisible = true;
+        }
+      }
+    });
+  }
 
   ngOnInit() {
   }
